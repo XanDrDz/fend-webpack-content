dc7976fcec14ceca5c1d4663e36d74cd133fa8e3
XanDrDz <alexandr.dzyub@gmail.com>
2020-09-07 22:27:56 +0500

    new code line

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-<<<<<<< HEAD
 const path = require("path")
 const webpack = require("webpack")
 module.exports = {
     entry: './src/client/index.js'
-=======
-const path = require("path")
-const webpack = require("webpack")
-module.exports = {
->>>>>>> 523510a4639d1a4765ebb31937d4cb2307f5fe8c
+}
+module: {
+    rules: [
+            {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+            }
+    ]
 }
